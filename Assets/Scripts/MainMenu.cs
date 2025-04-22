09c16dd612da918aaa6a994d50dcbe510eccd4ea
Victor <11018774@stud.hochschule-heidelberg.de>
2025-04-22 16:47:23 -0600

    Button re-uploaded, Added Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void StartGame() {
+      SceneManager.LoadSceneAsync(1);
+   } 
+   
+   public void ExitGame() {
+      Application.Quit();
+   }
+}
